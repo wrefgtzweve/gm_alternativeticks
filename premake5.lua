1db43cd93c5d0f4a704d305d8162124a06fde353
Redox <mail@redox.to>
2023-09-15 19:09:59 +0100

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+PROJECT_GENERATOR_VERSION = 3
+
+local options = {
+    trigger = "gmcommon",
+    description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+    value = "path to garrysmod_common directory"
+}
+local filesToInclude = {
+    "src/**.cpp",
+    "src/**.hpp",
+    "src/**.h"
+}
+local workspace = { name = "alternativeticks" }
+
+newoption( options )
+
+local gmcommon = assert( _OPTIONS.gmcommon or os.getenv( "GARRYSMOD_COMMON" ), "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory" )
+include( gmcommon )
+
+CreateWorkspace( workspace )
+
+CreateProject( { serverside = true } )
+    IncludeLuaShared()
+    IncludeHelpersExtended()
+    IncludeDetouring()
+    IncludeScanning()
+
+    IncludeSDKCommon()
+    IncludeSDKTier0()
+    IncludeSDKTier1()
+    IncludeSDKTier2()
+    IncludeSDKTier3()
+    IncludeSDKMathlib()
+    IncludeSDKRaytrace()
+
+    files( filesToInclude )
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <GarrysMod/Lua/Interface.h>
+#include <GarrysMod/FactoryLoader.hpp>
+#include <GarrysMod/Symbol.hpp>
+
+#include <scanning/symbolfinder.hpp>
+#include <detouring/hook.hpp>
+
+#ifdef SYSTEM_LINUX
+	#ifdef ARCHITECTURE_X86
+		Symbol symbol = Symbol::FromName("_ZN11CBaseEntity28IsSimulatingOnAlternateTicksEv");
+	#elif ARCHITECTURE_X86_64
+		Symbol symbol = Symbol::FromName("_ZNK11CBaseEntity28IsSimulatingOnAlternateTicksEv");
+	#endif
+	#elif SYSTEM_WINDOWS
+	#ifdef ARCHITECTURE_X86
+		Symbol symbol = Symbol::FromName("?IsSimulatingOnAlternateTicks@CBaseEntity@@QBE_NXZ");
+	#elif ARCHITECTURE_X86_64
+		Symbol symbol = Symbol::FromName("?IsSimulatingOnAlternateTicks@CBaseEntity@@QEBA_NXZ");
+	#endif
+#endif
+
+Detouring::Hook AlternateTicks_detour;
+
+bool IsSimulatingOnAlternateTicks_detour(void* thisptr) {
+	return true;
+}
+
+GMOD_MODULE_OPEN() {
+	SourceSDK::ModuleLoader server_srv("server_srv");
+	SymbolFinder symfinder;
+
+	void* is_simulating_on_alternate_ticks = nullptr;
+	is_simulating_on_alternate_ticks = symfinder.Resolve(server_srv.GetModule(), symbol.name.c_str(), symbol.length);
+
+	if (is_simulating_on_alternate_ticks == nullptr) {
+		LUA->ThrowError("Failed to find IsSimulatingOnAlternateTicks");
+		return 0;
+	}
+
+	bool* is_simulating_on_alternate_ticks_ptr = reinterpret_cast<bool*>(is_simulating_on_alternate_ticks);
+
+	AlternateTicks_detour.Create(is_simulating_on_alternate_ticks_ptr, IsSimulatingOnAlternateTicks_detour );
+
+	return 1;
+}
+
+GMOD_MODULE_CLOSE() {
+	AlternateTicks_detour.Destroy();
+	return 0;
+}
